8a88e0bd9bd9f531c6bbe69c154e45722323cd64
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-01-09 13:36:11 -0500

    moved code into a main function

--- wordle.py
+++ wordle.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 from random import choice
 from pathlib import Path
-from sys import stdin, stdout
+from sys import stdin, stdout, stderr
 
-word_list = Path('dict').read_text().split()
-word = choice(word_list).strip()
 
-for i in range(6):
-	guess=''
-	while True:
-		print(f'{i}/6:', end='')
-		stdout.flush()
-		guess = stdin.readline().strip()
-		if guess not in word_list:
-			print(f"'{guess}' not in word list")
-		else:
+def word_delta(word1, word2):
+	return ''.join(
+		g if g==l else
+		'+' if g in word2 else
+		'-' for (g,l) in zip(word1, word2)
+	)
+
+def main():
+	word_list = Path('dict').read_text().split()
+	word = choice(word_list).strip()
+
+	for i in range(6):
+		guess=''
+		while True:
+			print(f'{i}/6:', end='', file=stderr)
+			stderr.flush()
+			guess = stdin.readline().strip()
+			if guess not in word_list:
+				print(f"'{guess}' not in word list", file=stderr)
+			else:
+				break
+
+		print(word_delta(guess, word), file=stdout)
+		if guess == word:
+			print("you win!", file=stderr)
 			break
 
-	print(*(g if g==l else '+' if g in word else '-' for (g,l) in zip(guess, word)), sep='' )
-	if guess == word:
-		print("you win!")
-		break
+	print(word)
 
-print(word)
+if __name__ == '__main__':
+	main()
