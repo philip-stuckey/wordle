0e2d232caa3c8ea57b49ccd24c255440164347ba
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-01-18 12:18:21 -0500

    tried a different metric

--- unwordle.py
+++ unwordle.py
@@ -3,12 +3,20 @@ from wordle import word_delta
 from pathlib import Path
 from sys import stdin, stdout, stderr
 from argparse import ArgumentParser
+from statistics import mean
+
+def freq(data):
+	return [sum(map(lambda x: x==y, data)) for y in set(data)]
+
+def negative_mean_group_size(word, word_list):
+	return -mean(freq(list(map(lambda w: word_delta(word, w), word_list))))
 
 def count_unique_results(word, word_list):
 	return len(set(map(lambda w: word_delta(word, w), word_list)))
 
 def score(word, word_list):
-	return count_unique_results(word,word_list)
+	return negative_mean_group_size(word, word_list)
+#	return count_unique_results(word,word_list)
 
 def unwordle(word_list, guess, score=count_unique_results, input=stdin, output=stdout):
 	if guess is None:
