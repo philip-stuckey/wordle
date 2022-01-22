417cff40430483e022569286a1d15cfa4faac91e
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-01-22 13:27:23 -0500

    added # to some output to make datamashing easier

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
 test -p result && rm result;
-echo "generating first guess (this may take some time)"
+echo "# generating first guess (this may take some time)"
 
 firstguess=$(./unwordle.py < /dev/null 2> /dev/null | head -n1)
-echo $firstguess
+echo "# first guess $firstguess"
 
+echo word attempts
 while read word
 do
+	# echo "# $word"
 	mkfifo result
 	./unwordle.py -g $firstguess < result | ./wordle.py -w $word -n 100  >> result 2> /dev/null
 	rm result;
