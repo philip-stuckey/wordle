eb8c2f15a552ee6f9f8581bd815fa187c6a2748a
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-02-04 23:30:52 -0500

    switched back to python file for tests

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 test -p result && rm result;
 echo "# generating first guess (this may take some time)"
 
-firstguess=$(./unwordle.jl < /dev/null 2> /dev/null | head -n1)
+firstguess=tares  #$(./unwordle.py < /dev/null 2> /dev/null | head -n1)
 echo "# first guess $firstguess"
 
 echo word attempts
@@ -11,7 +11,7 @@ while read word
 do
 	# echo "# $word"
 	mkfifo result
-	./unwordle.jl -g $firstguess < result | ./wordle.py -w $word -n 100  >> result 2> /dev/null
+	./unwordle.py -g $firstguess < result | ./wordle.py -w $word -n 100  >> result 2> /dev/null
 	rm result;
 	sleep 0.125
 done < wordles.txt
